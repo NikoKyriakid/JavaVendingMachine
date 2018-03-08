f46dedb8af3da51c958b2cf2f2e2b1afc492d7b8
Niko Kyriakidis <nick.kyriakid@gmail.com>
2018-03-08 22:45:13 +0000

    User input interface improved.

--- src/com/nikokyriakid/VendingMachine/startVM.java
+++ src/com/nikokyriakid/VendingMachine/startVM.java
@@ -7,73 +7,90 @@ import java.util.Scanner;
 
 public class startVM {
     public static void main(String[] args) {
-        int slot, quantity, input;
+        int slot, quantity, input, slotSize = 10;
         double price;
+        boolean alive = true;
         String coins, ary[];
-        VendingMachineImpl vm = new VendingMachineImpl(10, Arrays.asList(0.10, 0.20, 0.50, 1.0, 2.0));
+        VendingMachineImpl vm = new VendingMachineImpl(slotSize, Arrays.asList(0.10, 0.20, 0.50, 1.0, 2.0));
 
-        System.out.println("=============== Vending Machine by NK =================");
+        System.out.println("================ Vending Machine by NK =================");
         Scanner reader = new Scanner(System.in);
-        while(true) {
-            System.out.println("=======================================================");
+        while(alive) {
+            System.out.println("========================================================");
             System.out.println("1. Set Quantity   | 2. Set Price      | 3. Buy Product\n" +
                                 "4. Add Coins      | 5. Print Slots    | 6. Print Change\n" +
-                                "7. Populate Slots | 8. Populate Coins | 9. Reset");
+                                "7. Populate Slots | 8. Populate Coins | 9. Reset | 0. Exit");
             System.out.print("Select an action: ");
 
-            input = reader.nextInt();
-            if (input == 0) {
-                System.out.print("App terminated.");
-                break;
-            }
-            else if (input == 1) {
-                System.out.print("Type slot: ");
-                slot = reader.nextInt();
-                System.out.print("Type quantity: ");
-                quantity = reader.nextInt();
-                vm.setQuantity(slot, quantity);
-            } else if (input == 2) {
-                System.out.print("Type slot: ");
-                slot = reader.nextInt();
-                System.out.print("Type Price: ");
-                price = reader.nextDouble();
-                vm.setPrice(slot, price);
-            } else if (input == 3) {
-                System.out.print("Type slot: ");
-                slot = reader.nextInt();
-                System.out.print("Put Coins: ");
-                coins = reader.next();
-                ary = coins.split(",");
-
-                try {
-                    vm.buyProduct(slot, Arrays.asList(ary).stream().map(s -> Double.parseDouble(s)).collect(toList()));
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (input == 4) {
-                System.out.print("Type Coins: ");
-                coins = reader.next();
-                ary = coins.split(",");
-                try {
-                    vm.addCoinsByList(Arrays.asList(ary).stream().map(s -> Double.parseDouble(s)).collect(toList()), true);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            } else if (input == 5) {
-                vm.displaySlots();
-            } else if (input == 6) {
-                vm.displayCoins();
-            } else if (input == 7) {
-                startVM.populateDefaults(vm);
-            } else if (input == 8) {
-                startVM.populateCoins(vm);
-            } else if (input == 9) {
-                vm.reset();
-                System.out.println("Vending Machine is reset.");
-            } else {
-                System.out.println("No valid action.");
+            try {
+                input = reader.nextInt();
+            } catch (Exception e) {
+                reader.nextLine();
+                input = -1; // Will default to non valid action.
             }
 
+            switch (input) {
+                case 0:
+                    System.out.print("App terminated.");
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.print("Type slot (0-9): ");
+                    slot = reader.nextInt();
+                    System.out.print("Type quantity (0-9): ");
+                    quantity = reader.nextInt();
+                    vm.setQuantity(slot, quantity);
+                    break;
+                case 2:
+                    System.out.print("Type slot (0-9): ");
+                    slot = reader.nextInt();
+                    System.out.print("Type Price (eg 1.2): ");
+                    price = reader.nextDouble();
+                    vm.setPrice(slot, price);
+                    break;
+                case 3:
+                    System.out.print("Type slot (0-9): ");
+                    slot = reader.nextInt();
+                    System.out.print("Put Coins (eg. 1,1,2,0.5,0.2): ");
+                    coins = reader.next();
+                    ary = coins.split(",");
+
+                    try {
+                        vm.buyProduct(slot, Arrays.asList(ary).stream().map(s -> Double.parseDouble(s)).collect(toList()));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    System.out.print("Type Coins (eg. 1,1,2,0.5,0.2): ");
+                    coins = reader.next();
+                    ary = coins.split(",");
+                    try {
+                        vm.addCoinsByList(Arrays.asList(ary).stream().map(s -> Double.parseDouble(s)).collect(toList()), true);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 5:
+                    vm.displaySlots();
+                    break;
+                case 6:
+                    vm.displayCoins();
+                    break;
+                case 7:
+                    startVM.populateDefaults(vm);
+                    break;
+                case 8:
+                    startVM.populateCoins(vm);
+                    break;
+                case 9:
+                    vm.reset();
+                    System.out.println("Vending Machine is reset.");
+                    break;
+                default:
+                    System.out.println("No valid action. Input must be an integer number between 0-9");
+                    break;
+            }
         }
         reader.close();
     }
